f3b1e5e2cb70e6694865373199d02b4b5bc8ccad
Prince Kyeremeh <kyeremehprince68@gmail.com>
2024-06-04 13:43:22 +0000

    Optimise imports

--- app/src/main/java/dev/android/innerinsight/screens/HomeScreen.kt
+++ app/src/main/java/dev/android/innerinsight/screens/HomeScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
